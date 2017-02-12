aab9fe7a5ea692e027cc8c85c694022883d3c920
Khaled <khaled.alqerem@gmail.com>
2017-02-12 15:55:08 +0300

    Add progress bar

--- app/src/main/java/com/example/khalk/network2/MainActivity.java
+++ app/src/main/java/com/example/khalk/network2/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.khalk.network2;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -21,45 +26,64 @@ public class MainActivity extends AppCompatActivity {
     //final static String BASE_URL ="http://192.168.1.4:8888/SensoryBoxAPK/AudioVolume/0.8";
     final static String BaseUrl = "http://192.168.1.4";
     final static String sensoryBox = "/SensoryBoxAPK/";
-    Button AudioVolume_08_Button;
-    TextView AudioVolume_08_ResultTextView;
-    Button Languge_en_Button;
-    TextView Languge_en_ResultTextView;
-    Button Languge_ar_Button;
-    TextView Languge_ar_ResultTextView;
+    Button audioVolume08Button;
+    TextView audioVolume08ResultTextView;
+    Button langugeEnButton;
+    TextView langugeEnResultTextView;
+    Button langugeArButton;
+    TextView langugeArResultTextView;
+    Button resetButton;
+    Button testAllButton;
+    private ProgressBar loadingIndicator;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AudioVolume_08_Button=(Button)findViewById(R.id.audioVolume_0_8_Button);
-        AudioVolume_08_ResultTextView=(TextView)findViewById(R.id.audioVolume_0_8_textResult);
-        Languge_en_Button=(Button)findViewById(R.id.lang_en_Button);
-        Languge_en_ResultTextView=(TextView)findViewById(R.id.lang_en_textResult);
-        Languge_ar_Button=(Button)findViewById(R.id.lang_ar_Button);
-        Languge_ar_ResultTextView=(TextView)findViewById(R.id.lang_ar_textResult);
+        audioVolume08Button =(Button)findViewById(R.id.audioVolume_0_8_Button);
+        audioVolume08ResultTextView =(TextView)findViewById(R.id.audioVolume_0_8_textResult);
+        langugeEnButton =(Button)findViewById(R.id.lang_en_Button);
+        langugeEnResultTextView =(TextView)findViewById(R.id.lang_en_textResult);
+        langugeArButton =(Button)findViewById(R.id.lang_ar_Button);
+        langugeArResultTextView =(TextView)findViewById(R.id.lang_ar_textResult);
+        resetButton=(Button)findViewById(R.id.resetButton);
+        testAllButton=(Button)findViewById(R.id.testAllButton);
+        loadingIndicator = (ProgressBar) findViewById(R.id.loading_indicator);
+
         client = new OkHttpClient();
 
-        AudioVolume_08_Button.setOnClickListener(new View.OnClickListener() {
+        audioVolume08Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TestUrl("8888", sensoryBox, "AudioVolume", "0.8","200",AudioVolume_08_ResultTextView.getId());
+                TestUrl("8888", sensoryBox, "AudioVolume", "0.8","200", audioVolume08ResultTextView.getId());
+                Log.d(TAG, "onClick: ");
+                
             }
         });
 
-        Languge_en_Button.setOnClickListener(new View.OnClickListener() {
+        langugeEnButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODO: 2/12/2017 prevent multiple button press , make it after post execute-after request-
-                TestUrl("8888", sensoryBox, "ChangeLanguage", "en","200",Languge_en_ResultTextView.getId());
+                TestUrl("8888", sensoryBox, "ChangeLanguage", "en","200", langugeEnResultTextView.getId());
             }
         });
 
-        Languge_ar_Button.setOnClickListener(new View.OnClickListener() {
+        langugeArButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                TestUrl("8888", sensoryBox, "ChangeLanguage", "ar","200",Languge_ar_ResultTextView.getId());
+                TestUrl("8888", sensoryBox, "ChangeLanguage", "ar","200", langugeArResultTextView.getId());
+            }
+        });
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                audioVolume08ResultTextView.setText("");
+                langugeArResultTextView.setText("");
+                langugeEnResultTextView.setText("");
             }
         });
 
@@ -79,9 +103,9 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    public class UrlTesting extends AsyncTask<String, Void, String> {
+    public class UrlTesting extends AsyncTask<String, Void, String> implements DialogInterface.OnCancelListener {
         private String aaa = null;
-
+        private Dialog dialog = null;
         private TextView resutlTextView;
 
         UrlTesting(String code,int TextViewID) {
@@ -90,9 +114,24 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
+        protected void onPreExecute() {
+            // To disable the whole screen --> setCancelable(false);
+            dialog = new Dialog(MainActivity.this, android.R.style.Theme_Translucent_NoTitleBar_Fullscreen);
+//            dialog = new Dialog(MainActivity.this, android.R.style.Theme_Black);
+            dialog.setCancelable(false);
+            loadingIndicator.setVisibility(View.VISIBLE);
+            dialog.show();
+
+           // super.onPreExecute();
+        }
+
+        @Override
         protected String doInBackground(String... params) {
             String UrlTestingUrl = params[0];
             String githubUrlTestingResults = null;
+
+            dialog.dismiss();
+
 
             try {
                 Request request = new Request.Builder()
@@ -103,18 +142,28 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "doInBackground: " + githubUrlTestingResults);
             } catch (IOException e) {
                 // TODO: 2/12/2017 handle 3 status: pass. fail. prepare_fail
+                githubUrlTestingResults="existException"+e.getStackTrace().toString();
                 e.printStackTrace();
             }
+
 
             return githubUrlTestingResults;
         }
 
+
+
         // COMPLETED (3) Override onPostExecute to display the results in the TextView
         @Override
         protected void onPostExecute(String githubUrlTestingResults) {
+            loadingIndicator.setVisibility(View.INVISIBLE);
             if (githubUrlTestingResults != null && !githubUrlTestingResults.equals("")) {
-                System.out.print(githubUrlTestingResults);
-                testResponce(githubUrlTestingResults, this.aaa);
+                if(githubUrlTestingResults.startsWith("existException")){
+                    resutlTextView.setText("prepare_fail");
+                } else {
+                    System.out.print(githubUrlTestingResults);
+                    testResponce(githubUrlTestingResults, this.aaa);
+                }
+
 
             }
         }
@@ -122,12 +171,17 @@ public class MainActivity extends AppCompatActivity {
         public void testResponce(String responceCode, String expectedCode) {
             if (responceCode.equals(expectedCode)) {
                 Log.d(TAG, "onPostExecute: right");
-                resutlTextView.setText("OK");
+                resutlTextView.setText("pass");
             } else {
                 Log.d(TAG, "onPostExecute: false");
-                resutlTextView.setText("ERROR");
+                resutlTextView.setText("fail");
             }
 //            this.aaa = responceCode;
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+            cancel(true);
         }
     }
 
