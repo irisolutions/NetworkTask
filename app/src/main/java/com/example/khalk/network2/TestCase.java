ef097eae89db1794e1aa87089998b7d3af6d1323
Khaled <khaled.alqerem@gmail.com>
2017-02-13 15:22:34 +0200

    Show results completly

--- app/src/main/java/com/example/khalk/network2/TestCase.java
+++ app/src/main/java/com/example/khalk/network2/TestCase.java
@@ -26,13 +26,14 @@ public class TestCase {
     private String testName;
     private String expectedCode;
     private String testResult;
+    public  String finalUrl;
+    public String solidUrl;
     public String baseUrl;
     public String port;
-    private Boolean Testing=false;
-    private static final String TAG = TestCase.class.getName();
-    OkHttpClient client;
     public ProgressBar loadingIndicator;
     public TextView resultTextView;
+    public View parent;
+    public OkHttpClient client;
 
 
 
@@ -40,9 +41,11 @@ public class TestCase {
     TestCase(String caseName,String expectedCode){
         this.testName=caseName;
         this.expectedCode=expectedCode;
-        client=client = new OkHttpClient();
         port="8888";
-        baseUrl="192.168.1.4";
+        baseUrl="http://192.168.1.2";
+        solidUrl="192.168.1.2";
+        finalUrl=baseUrl+":"+port+testName;
+        client = new OkHttpClient();
     }
 
 
@@ -73,12 +76,7 @@ public class TestCase {
     public void setTestResult(String result){
         this.testResult=result;
     }
-    public Boolean isTesting(){
-        return Testing;
-    }
-    public void setTesting(Boolean test){
-        this.Testing=test;
-    }
+
     public void setLoadingIndicator(ProgressBar loading){
         this.loadingIndicator=loading;
     }
@@ -88,210 +86,9 @@ public class TestCase {
     public TextView getResultTextView(){
         return resultTextView;
     }
-
-
-    /**
-     * this is class to test case
-     */
-   public void run(){
-       String finalUrl = baseUrl + ":" + port + "/"+testName;
-//        String finalUrl="http://192.168.1.4:8888/SensoryBoxAPK/AudioVolume/0.8";
-
-        ResultData expectedResultData=new ResultData();
-        expectedResultData.setCode(Integer.parseInt(expectedCode));
-//        Log.d(TAG, "TestUrl: INteget.parseInt("+expectedResultData.getCode()+")");
-        new UrlTesting(expectedResultData).execute(finalUrl);
-   }
-
-
-    public class UrlTesting extends AsyncTask<Object, Object, ResultData> implements DialogInterface.OnCancelListener {
-        private ResultData aaa = null;
-        private Dialog dialog = null;
-        //private TextView resutlTextView;
-        private TextView pingView;
-        String pingResult="";
-        Socket socket;
-        InetAddress in;
-
-
-
-
-
-        UrlTesting(ResultData code) {
-            this.aaa = code;
-            in=null;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            // To disable the whole screen --> setCancelable(false);
-            setTesting(true);
-
-//            dialog = new Dialog(MainActivity.this, android.R.style.Theme_Translucent_NoTitleBar_Fullscreen);
-//            dialog = new Dialog(MainActivity.this, android.R.style.Theme_Black);
-//            pingView=(TextView)findViewById(R.id.pingText);
-//            dialog.setCancelable(false);
-            loadingIndicator.setVisibility(View.VISIBLE);
-//            dialog.show();
-
-           // super.onPreExecute();
-        }
-
-
-        @Override
-        protected ResultData doInBackground(Object... params) {
-            //Object UrlTestingUrl = params[0];
-
-            String UrlTestingUrl= (String) params[0];
-            ResultData data = new ResultData();
-           // data=null;
-            NetworkTestResult preFailSocketUnreachable = NetworkTestResult.PRE_FAIL_SOCKET_UNREACHABLE;
-//            dialog.dismiss();
-
-
-
-            /**
-             * check if ip address is rechable
-             */
-            try {
-//                this.in =InetAddress.getByName("http://192.168.1.4");
-                in = InetAddress.getByName("192.168.1.4");
-            } catch (UnknownHostException e) {
-                e.printStackTrace();
-                data.setStatus("prefail");
-                data.setException(e);
-                return data;
-            }
-
-            try {
-                if (in.isReachable(10000)) {
-                    Log.d(TAG, "doInBackground: ip address reachable");
-                }
-            } catch (IOException e) {
-                Log.d(TAG, "doInBackground: ip address unreachable");
-                e.printStackTrace();
-                data.setStatus("prefail");
-                data.setException(e);
-                return data;
-            }
-            /**
-             * check if socket rechable
-             */
-            try (Socket sSocket = new Socket()) {
-                InetSocketAddress sa = new InetSocketAddress("192.168.1.4", 8888);
-
-                sSocket.connect(sa, 1000);           // --> change from 1 to 500 (for example)
-                // ss.close();
-                Log.d(TAG, "doInBackground: socket is rechable");
-
-            } catch (Exception e) {
-                Log.d(TAG, "doInBackground: socket is unreachable");
-                data.setStatus("prefail");
-                data.setException(e);
-                return data;
-//                preFailSocketUnreachable = NetworkTestResult.PRE_FAIL_SOCKET_UNREACHABLE;
-            }
-
-
-            ResultData resultData = new ResultData();
-
-            try {
-                Request request = new Request.Builder()
-                        .url(UrlTestingUrl)
-                        .build();
-
-// ensure the response (and underlying response body) is closed
-                try (Response response = client.newCall(request).execute()) {
-//                    data = String.valueOf(response.code());
-                    data.setCode(response.code());
-                    resultData.setCode(response.code());
-                    testResponce(data, this.aaa);
-
-                    Log.d(TAG, "doInBackground: " + data);
-                    return data;
-                }
-
-            } catch (IOException e) {
-                // TODO: 2/12/2017 handle 3 status: pass. fail. prepare_fail
-                data.setStatus("prefail");
-                data.setException(e);
-                return data;
-            }
-
-        }
-
-        // COMPLETED (3) Override onPostExecute to display the results in the TextView
-        @Override
-        protected void onPostExecute(ResultData resultData) {
-            loadingIndicator.setVisibility(View.INVISIBLE);
-            setTesting(false);
-            if (resultData != null && !resultData.equals("")) {
-                if(resultData.exception != null){
-                    setTestResult("pre_fail");
-
-                } else {
-                    System.out.print(resultData);
-//                    testResponce(resultData, this.aaa);
-                }
-
-
-            }
-            getResultTextView().setText(getTestResult());
-//            this.resutlTextView.setText("aaaa");
-        }
-
-        public void testResponce(ResultData responceCode, ResultData expectedCode) {
-            if (responceCode.equals(expectedCode)) {
-                Log.d(TAG, "onPostExecute: right");
-                setTestResult("pass");
-                getResultTextView().setText(getTestResult());
-            } else {
-                Log.d(TAG, "onPostExecute: false");
-                setTestResult("fail");
-                getResultTextView().setText(getTestResult());
-
-            }
-
-        }
-
-        @Override
-        public void onCancel(DialogInterface dialog) {
-            cancel(true);
-        }
+    public void setParent(View view){
+        this.parent=view;
     }
-
-
-    private class ResultData {
-        private int code;
-        private Exception exception;
-        private String status;
-
-        public void setCode(int code) {
-            this.code = code;
-        }
-
-        public int getCode() {
-            return code;
-        }
-
-        public void setException(Exception exception) {
-            this.exception = exception;
-        }
-
-        public Exception getException() {
-            return exception;
-        }
-
-        public void setStatus(String status) {
-            this.status = status;
-        }
-
-        public String getStatus() {
-            return status;
-        }
-    }
-
-
 
 }
 
--- app/src/main/java/com/example/khalk/network2/caseAdapter.java
+++ app/src/main/java/com/example/khalk/network2/caseAdapter.java
@@ -4,7 +4,6 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
-import android.support.annotation.NonNull;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,15 +12,12 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-import java.util.List;
-
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -34,6 +30,9 @@ import okhttp3.Response;
 public class CaseAdapter extends ArrayAdapter<TestCase> {
     private static final String TAG = CaseAdapter.class.getName();
     private ProgressBar loadingIndicator;
+    private TextView resultTestTextView;
+    private Boolean Testing=false;
+//    TestCase testCase;
 
 
     public CaseAdapter(Context context, ArrayList<TestCase> testCases) {
@@ -50,27 +49,28 @@ public class CaseAdapter extends ArrayAdapter<TestCase> {
                     R.layout.list_item,parent,false);
         }
 
-         final TestCase testCase=getItem(position);
+        final TestCase testCase=getItem(position);
 
         TextView nameTestTextView=(TextView)listItemView.findViewById(R.id.case_name);
         nameTestTextView.setText(testCase.getTestName());
+//        testCase.setParentView(listItemView);
 
         loadingIndicator = (ProgressBar) listItemView.findViewById(R.id.loading_indicator);
         testCase.setLoadingIndicator(loadingIndicator);
         Button testButton=(Button)listItemView.findViewById(R.id.test_button);
-        final TextView resultTestTextView=(TextView)listItemView.findViewById(R.id.test_result);
+        resultTestTextView=(TextView)listItemView.findViewById(R.id.test_result);
         testCase.setResultTextView(resultTestTextView);
-
+        //testCase.setParent(parent);
 
 
         testButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                run(testCase);
+                if(!Testing){
+                    run(testCase);
+                }
 
-
-               testCase.run();
-               resultTestTextView.setText(testCase.getTestResult());
+//                testCase.run();
             }
         });
 
@@ -83,6 +83,7 @@ public class CaseAdapter extends ArrayAdapter<TestCase> {
 //        String finalUrl="http://192.168.1.4:8888/SensoryBoxAPK/AudioVolume/0.8";
         ResultData expectedResultData=new ResultData();
         expectedResultData.setCode(Integer.parseInt(test.getExpectedCode()));
+        Log.d(TAG, "run: expected code from class TestCase is "+test.getExpectedCode());
 //        Log.d(TAG, "TestUrl: INteget.parseInt("+expectedResultData.getCode()+")");
         new UrlTesting(expectedResultData,test).execute(finalUrl);
     }
@@ -115,7 +116,7 @@ public class CaseAdapter extends ArrayAdapter<TestCase> {
 //            dialog = new Dialog(MainActivity.this, android.R.style.Theme_Black);
 //            pingView=(TextView)findViewById(R.id.pingText);
 //            dialog.setCancelable(false);
-             loadingIndicator.setVisibility(View.VISIBLE);
+             testCase.loadingIndicator.setVisibility(View.VISIBLE);
 //            dialog.show();
 
             // super.onPreExecute();
@@ -125,10 +126,11 @@ public class CaseAdapter extends ArrayAdapter<TestCase> {
         @Override
         protected ResultData doInBackground(Object... params) {
             //Object UrlTestingUrl = params[0];
+            Testing=true;
 
             String UrlTestingUrl= (String) params[0];
             ResultData data = new ResultData();
-            // data=null;
+//             data=null;
             NetworkTestResult preFailSocketUnreachable = NetworkTestResult.PRE_FAIL_SOCKET_UNREACHABLE;
 //            dialog.dismiss();
 
@@ -142,7 +144,8 @@ public class CaseAdapter extends ArrayAdapter<TestCase> {
                 in = InetAddress.getByName("192.168.1.4");
             } catch (UnknownHostException e) {
                 e.printStackTrace();
-                data.setStatus("prefail");
+                data.setStatus("prefail ==> UnknownHostException");
+                testCase.setTestResult(data.getStatus().toString());
                 data.setException(e);
                 return data;
             }
@@ -154,7 +157,8 @@ public class CaseAdapter extends ArrayAdapter<TestCase> {
             } catch (IOException e) {
                 Log.d(TAG, "doInBackground: ip address unreachable");
                 e.printStackTrace();
-                data.setStatus("prefail");
+                data.setStatus("prefail ==> unreachable IP address");
+                testCase.setTestResult(data.getStatus().toString());
                 data.setException(e);
                 return data;
             }
@@ -162,7 +166,7 @@ public class CaseAdapter extends ArrayAdapter<TestCase> {
              * check if socket rechable
              */
             try (Socket sSocket = new Socket()) {
-                InetSocketAddress sa = new InetSocketAddress("192.168.1.4", 8888);
+                InetSocketAddress sa = new InetSocketAddress("192.168.1.2", Integer.parseInt(testCase.port));
 
                 sSocket.connect(sa, 1000);           // --> change from 1 to 500 (for example)
                 // ss.close();
@@ -170,7 +174,8 @@ public class CaseAdapter extends ArrayAdapter<TestCase> {
 
             } catch (Exception e) {
                 Log.d(TAG, "doInBackground: socket is unreachable");
-                data.setStatus("prefail");
+                data.setStatus("prefail ==> un reachable Socket");
+                testCase.setTestResult(data.getStatus().toString());
                 data.setException(e);
                 return data;
 //                preFailSocketUnreachable = NetworkTestResult.PRE_FAIL_SOCKET_UNREACHABLE;
@@ -186,9 +191,10 @@ public class CaseAdapter extends ArrayAdapter<TestCase> {
 
 // ensure the response (and underlying response body) is closed
                 try (Response response = testCase.client.newCall(request).execute()) {
-//                    data = String.valueOf(response.code());
+//                     String currResponceCode=String.valueOf(response.code());
                     data.setCode(response.code());
-                    resultData.setCode(response.code());
+                    Log.d(TAG, "doInBackground: this is responce code in try catch");
+//                    resultData.setCode(response.code());
                     testResponce(data, this.aaa);
 
                     Log.d(TAG, "doInBackground: " + data);
@@ -197,7 +203,8 @@ public class CaseAdapter extends ArrayAdapter<TestCase> {
 
             } catch (IOException e) {
                 // TODO: 2/12/2017 handle 3 status: pass. fail. prepare_fail
-                data.setStatus("prefail");
+                data.setStatus("prefail ==> no responce");
+                testCase.setTestResult(data.getStatus().toString());
                 data.setException(e);
                 return data;
             }
@@ -207,31 +214,34 @@ public class CaseAdapter extends ArrayAdapter<TestCase> {
         // COMPLETED (3) Override onPostExecute to display the results in the TextView
         @Override
         protected void onPostExecute(ResultData resultData) {
-           loadingIndicator.setVisibility(View.INVISIBLE);
+            Testing=false;
+           testCase.loadingIndicator.setVisibility(View.INVISIBLE);
 //            setTesting(false);
             if (resultData != null && !resultData.equals("")) {
                 if(resultData.exception != null){
-//                    testCase.setTestResult("pre_fail");
+                    testCase.setTestResult("pre_fail");
 //
                 } else {
 
                     System.out.print(resultData);
-//                    testResponce(resultData, this.aaa);
+                   // testResponce(resultData, this.aaa);
                 }
 
             }
 //            resutlTextView.setText("pre_fail");
+            testCase.resultTextView.setText(testCase.getTestResult());
         }
 
         public void testResponce(ResultData responceCode, ResultData expectedCode) {
-            if (responceCode.equals(expectedCode)) {
+//            if (responceCode.equals(expectedCode.getCode())) {
+            if (responceCode.getCode()==expectedCode.getCode()) {
                 Log.d(TAG, "onPostExecute: right");
-//                resutlTextView.setText("pass");
                 testCase.setTestResult("pass");
+//                testCase.resultTextView.setText(testCase.getTestResult());
             } else {
                 Log.d(TAG, "onPostExecute: false");
-//                resutlTextView.setText("fail");
                 testCase.setTestResult("fail");
+//                testCase.resultTextView.setText(testCase.getTestResult());
             }
         }
 
