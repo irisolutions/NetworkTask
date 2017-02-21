743ce555ca9d2d596d04984cc49f9c7595c927e9
Khaled <khaled.alqerem@gmail.com>
2017-02-21 11:03:00 +0200

    -Add TestCaseContract and TestCaseDbHelper classes

--- app/src/main/java/com/example/khalk/network/data/TestCaseContract.java
+++ app/src/main/java/com/example/khalk/network/data/TestCaseContract.java
@@ -9,7 +9,7 @@ import android.content.ContentResolver;
 import android.provider.BaseColumns;
 
 /**
- * API Contract for the Pets app.
+ * API Contract for the tests app.
  */
 public final class TestCaseContract {
 
@@ -33,91 +33,77 @@ public final class TestCaseContract {
 
     /**
      * Possible path (appended to base content URI for possible URI's)
-     * For instance, content://com.example.android.pets/pets/ is a valid path for
-     * looking at pet data. content://com.example.android.pets/staff/ will fail,
+     * For instance, content://com.example.khalk.network/tests is a valid path for
+     * looking at test data. content://com.example.khalk.network/staff/ will fail,
      * as the ContentProvider hasn't been given any information on what to do with "staff".
      */
-    public static final String PATH_PETS = "pets";
+    public static final String PATH_TESTS = "tests";
 
     /**
-     * Inner class that defines constant values for the pets database table.
-     * Each entry in the table represents a single pet.
+     * Inner class that defines constant values for the tests database table.
+     * Each entry in the table represents a single test.
      */
-    public static final class PetEntry implements BaseColumns {
+    public static final class TestCaseEntry implements BaseColumns {
 
-        /** The content URI to access the pet data in the provider */
-        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
+        /** The content URI to access the test data in the provider */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_TESTS);
 
         /**
-         * The MIME type of the {@link #CONTENT_URI} for a list of pets.
+         * The MIME type of the {@link #CONTENT_URI} for a list of tests.
          */
         public static final String CONTENT_LIST_TYPE =
-                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TESTS;
 
         /**
-         * The MIME type of the {@link #CONTENT_URI} for a single pet.
+         * The MIME type of the {@link #CONTENT_URI} for a single test.
          */
         public static final String CONTENT_ITEM_TYPE =
-                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PETS;
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_TESTS;
 
-        /** Name of database table for pets */
-        public final static String TABLE_NAME = "pets";
+        /** Name of database table for tests */
+        public final static String TABLE_NAME = "tests";
 
         /**
-         * Unique ID number for the pet (only for use in the database table).
+         * Unique ID number for the test (only for use in the database table).
          *
          * Type: INTEGER
          */
         public final static String _ID = BaseColumns._ID;
 
         /**
-         * Name of the pet.
+         * Name of the test case.
          *
          * Type: TEXT
          */
-        public final static String COLUMN_PET_NAME ="name";
-
+        public final static String COLUMN_TEST_NAME ="name";
+        
         /**
-         * Breed of the pet.
+         * Controller for the test case
          *
          * Type: TEXT
          */
-        public final static String COLUMN_PET_BREED = "breed";
+        public final static String COLUMN_TEST_CONTROLLER = "controller";
 
         /**
-         * Gender of the pet.
+         * Para1 for the test case
          *
-         * The only possible values are {@link #GENDER_UNKNOWN}, {@link #GENDER_MALE},
-         * or {@link #GENDER_FEMALE}.
+         * Type: TEXT
+         */
+        public final static String COLUMN_TEST_PARA1 = "para1";
+
+        /**
+         * Para2 for the test case
          *
-         * Type: INTEGER
+         * Type: TEXT
          */
-        public final static String COLUMN_PET_GENDER = "gender";
-
+        public final static String COLUMN_TEST_PARA2 = "para2";
         /**
-         * Weight of the pet.
+         * Para3 for the test case
          *
-         * Type: INTEGER
+         * Type: TEXT
          */
-        public final static String COLUMN_PET_WEIGHT = "weight";
-
-        /**
-         * Possible values for the gender of the pet.
-         */
-        public static final int GENDER_UNKNOWN = 0;
-        public static final int GENDER_MALE = 1;
-        public static final int GENDER_FEMALE = 2;
-
-        /**
-         * Returns whether or not the given gender is {@link #GENDER_UNKNOWN}, {@link #GENDER_MALE},
-         * or {@link #GENDER_FEMALE}.
-         */
-        public static boolean isValidGender(int gender) {
-            if (gender == GENDER_UNKNOWN || gender == GENDER_MALE || gender == GENDER_FEMALE) {
-                return true;
-            }
-            return false;
-        }
+        public final static String COLUMN_TEST_PARA3 = "para3";
+        
     }
 
 }
